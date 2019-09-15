c3c4b893d185b697b953c6f413439720cbfedda6
bakatarekun <ckyamanaka@gmail.com>
2019-09-15 08:39:27 -0400

    after heroku deployment2

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.get('/api/customers', (req, res) => {
 });
 
 const port = 5000 || process.env.PORT;
+app.set('port', (process.env.PORT || 5000));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
