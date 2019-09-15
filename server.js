94603027ed402411d90ca8077b273e1f5f0536ef
bakatarekun <ckyamanaka@gmail.com>
2019-09-15 09:04:29 -0400

    make it better

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/api/customers', (req, res) => {
   res.json(customers);
 });
 
-const port = 5000 || process.env.PORT;
+const port =  process.env.POR ||5000;
 app.set('port', (process.env.PORT || 5000));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
