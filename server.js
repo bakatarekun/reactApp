175241b762d10066a5401a8faec6c9b53f16d86e
bakatarekun <ckyamanaka@gmail.com>
2019-09-15 09:30:55 -0400

    change 1

--- server.js
+++ server.js
@@ -12,8 +12,17 @@ app.get('/api/customers', (req, res) => {
   res.json(customers);
 });
 
-const port =  process.env.POR ||5000;
-app.set('port', (process.env.PORT || 5000));
+if(process.env.NODE_ENV === "production")
+{
+  app.use(express.static('client/build'));
+
+ app.get('*', (req, res) => {
+
+  rest.sendFile(path.resolove(__dirname, "client", "build", "index.html"))
+ })
+}
+const port =  process.env.POR || 5000;
+
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
