8e437bafbe93c8d982ad680ab3b2bfe85beea25b
bakatarekun <ckyamanaka@gmail.com>
2019-09-15 08:52:45 -0400

    test

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/api/customers', (req, res) => {
   res.json(customers);
 });
 
-const port = 5000 || process.env.PORT;
+ port = 5000 || process.env.PORT;
 app.set('port', (process.env.PORT || 5000));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
